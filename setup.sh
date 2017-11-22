78c8732ae81f44622e54891b6a0e73a66bcd80f8
Todd Edwards <todd@mtn.cc>
2017-11-22 01:58:37 -0500

    Cleaning up a bit more.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash -xe
-dest=${DOTFILES_HOME:-$HOME}
+#!/usr/bin/env bash
+set -e
 
 # Default umask on WSL is duuuuuuumb
 umask 0022
+
+dest=${DOTFILES_HOME:-$HOME}
 
 clone=$(mktemp -d)
 git clone https://github.com/triangletodd/dotfiles.git $clone
