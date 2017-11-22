3a207a09078ea1766cacb781051687cb017d9c13
Todd Edwards <todd@mtn.cc>
2017-11-22 01:56:01 -0500

    umask hack for WSL

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash -xe
 dest=${DOTFILES_HOME:-$HOME}
 
+# Default umask on WSL is duuuuuuumb
+umask 0022
+
 clone=$(mktemp -d)
 git clone https://github.com/triangletodd/dotfiles.git $clone
 rsync -av --exclude 'README.md' $clone/ $dest/
